3b0b0889f09a9d867b5b0079f3c528677207bc68
sofvillanueva <SV282442@fi365.ort.edu.uy>
2025-11-05 22:31:12 -0300

    H4 - FE: Agrega ruta para la direccion a la que envia la url del mail de cambio de password, modificaciones pequeñas al login, logica en el ForgetPassword para conectarse con el backend, agrega fetch a los dos endpoints del back

--- WinniElectricidad-Frontend/src/services/authService.js
+++ WinniElectricidad-Frontend/src/services/authService.js
@@ -1,7 +1,7 @@
 import ApiError from "./ApiError";
 
 //Login, Logout, Registro y recuperacion de contraseñas
-const urlAPI = "https://localhost:7059/WinniElectricidadApi/Usuario/"
+const urlAPI = "http://localhost:5269/WinniElectricidadApi/Usuario/"
 
 export const login = async (email, password) => {
     const response = await fetch(`${urlAPI}login`,{
@@ -35,4 +35,56 @@ export const login = async (email, password) => {
 export const logout = () => {
   localStorage.removeItem("token");
   localStorage.removeItem("role");
+};
+
+export const sendPasswordRecoveryEmail  = async (email) => {
+    try {
+    const response = await fetch(`${urlAPI}forgot-password`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email }),
+    });
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new ApiError(errorText || "Error desconocido al enviar el correo.", response.status);
+    }
+
+    const data = await response.json();
+    return data;
+
+  } catch (err) {
+    if (err instanceof TypeError) {
+      throw new Error("No se pudo conectar con el servidor. Verifica tu conexión o inténtalo más tarde.");
+    }
+
+    throw err;
+  }
+};
+
+export const resetPassword  = async (password, tokenPlain) => {
+    try {
+    const response = await fetch(`${urlAPI}reset-password`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ password, tokenPlain }),
+    });
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new ApiError(errorText || "Error desconocido.", response.status);
+    }
+
+    return await response.json();
+
+  } catch (err) {
+    if (err instanceof TypeError) {
+      throw new Error("No se pudo conectar con el servidor. Inténtalo más tarde.");
+    }
+    throw err;
+  }
 };
