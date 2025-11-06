83f05d07f768a3a9486ebb9d0c49910740b913a4
sofvillanueva <SV282442@fi365.ort.edu.uy>
2025-11-05 22:31:12 -0300

    H4 - FE: Agrega ruta para la direccion a la que envia la url del mail de cambio de password, modificaciones pequeñas al login, logica en el ForgetPassword para conectarse con el backend, agrega fetch a los dos endpoints del back

--- WinniElectricidad-Frontend/src/components/App/App.jsx
+++ WinniElectricidad-Frontend/src/components/App/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route, Navigate} from "react-router-dom";
 import Registro from "../../pages/Public/Registro/Registro.jsx";
 import Login from "../../pages/Public/Login/Login.jsx";
 import Principal from "../../pages/Public/Principal/Principal.jsx";
+import ResetPassword from "../../pages/Public/Login/ResetPassword";
 import { PrivateRoute } from "../routes/PrivateRoute.jsx";
 
 
@@ -16,6 +17,7 @@ export default function App() {
       {/*Paginas publicas */}
       <Route path="/registro" element={<Registro />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
 
       {/*Paginas privadas */}
       <Route
