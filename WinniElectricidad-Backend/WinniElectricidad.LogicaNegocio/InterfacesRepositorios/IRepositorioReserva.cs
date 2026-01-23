35073628946a82f33b7a65f994cdf0b3b4d1c9d4
carolinasosads <carolinasosads@gmail.com>
2026-01-22 21:00:25 -0300

    H125-Filtro de reservas. Cambio en metodos ya existentes

--- WinniElectricidad-Backend/WinniElectricidad.LogicaNegocio/InterfacesRepositorios/IRepositorioReserva.cs
+++ WinniElectricidad-Backend/WinniElectricidad.LogicaNegocio/InterfacesRepositorios/IRepositorioReserva.cs
@@ -8,10 +8,10 @@ public interface IRepositorioReserva : IRepositorio<Reserva>
     Task<bool> UsuarioTieneReservaEnHorario(int idUsuario, DateTime fechaReserva, CancellationToken ct = default);
     Task<bool> UsuarioTieneReservaEnDia(int idUsuario, DateTime fechaReserva, CancellationToken ct = default);
     Task<bool> HorarioOcupado(DateTime fechaReserva, CancellationToken ct = default);
-    Task<IReadOnlyList<Reserva>> FindAllFinalizadas(CancellationToken ct = default);
-    Task<IReadOnlyList<Reserva>> FindAllSegunEstado(EstadoReserva estado, CancellationToken ct = default);
+    Task<IReadOnlyList<Reserva>> FindAllFinalizadas(string? filtro, CancellationToken ct = default);
+    Task<IReadOnlyList<Reserva>> FindAllSegunEstado(EstadoReserva estado, string? filtro,  CancellationToken ct = default);
     Task<Reserva?> ObtenerReservaPorId(int id, CancellationToken ct = default);
-    Task<IReadOnlyList<Reserva>> FindHistoricoReservas(DateTime fechaMinima, DateTime fechaLimite, CancellationToken ct = default);
+    Task<IReadOnlyList<Reserva>> FindHistoricoReservas(DateTime fechaMinima, DateTime fechaLimite, string? filtro, CancellationToken ct = default);
     Task<IEnumerable<Reserva>> GetReservaSegunClienteId(int clienteId, CancellationToken ct = default);
     Task<IEnumerable<Reserva>> ObtenerReservasClienteConDetalle(int idCliente, CancellationToken ct = default);
 }
