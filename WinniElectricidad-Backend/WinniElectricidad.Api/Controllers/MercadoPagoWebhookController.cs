0edcb1194d4b213c0db87d65ead72c7655e70da0
carolinasosads <carolinasosads@gmail.com>
2026-02-03 20:37:41 -0300

    fix / namespace agregado

--- WinniElectricidad-Backend/WinniElectricidad.Api/Controllers/MercadoPagoWebhookController.cs
+++ WinniElectricidad-Backend/WinniElectricidad.Api/Controllers/MercadoPagoWebhookController.cs
@@ -3,6 +3,7 @@ using System.Text.Json;
 using System.Security.Cryptography;
 using System.Text;
 using WinniElectricidad.LogicaAplicacion.InterfacesServicios.Pago;
+namespace WinniElectricidad.Api.Controllers;
 
 [ApiController]
 [Route("WinniElectricidadApi/mercadopago")]
